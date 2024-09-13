8ae8607ad79b07db5e020ad4b948e9fe0a2628c3
Ravikiran <ravikiran.govind7@gmail.com>
2024-09-13 15:00:09 +0530

    fixing some errors

--- src/main/java/com/se/simplerepo/Controller.java
+++ src/main/java/com/se/simplerepo/Controller.java
@@ -1,15 +1,15 @@
 package com.se.simplerepo;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class Controller {
 
     @GetMapping("/getdata")
-    public ResponseEntity getData(){
-        return new ResponseEntity("Hello", HttpStatus.OK);
+    public ResponseEntity<String> getData(){
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
 
     }
+
 }
