33d38e224860d2225297cf08ff30f31715406d4c
Ravikiran <ravikiran.govind7@gmail.com>
2024-09-13 18:09:01 +0530

    fixing some errors

--- src/main/java/com/se/simplerepo/Controller.java
+++ src/main/java/com/se/simplerepo/Controller.java
@@ -3,6 +3,7 @@ package com.se.simplerepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class Controller {
 
@@ -11,5 +12,11 @@ public class Controller {
         System.out.println("test endpoint");
         return new ResponseEntity<>("Hello", HttpStatus.OK);
     }
+
+    @PostMapping("/getEmployeeDetails")
+    public ResponseEntity<String> getEmployeeDetails(){
+        return new ResponseEntity<>("Hello Employee",HttpStatus.OK);
+    }
+
 
 }
