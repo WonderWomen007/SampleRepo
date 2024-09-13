b9eefb270da245da38d6f4818aece9eef6a754b1
Ravikiran <ravikiran.govind7@gmail.com>
2024-09-13 11:39:13 +0530

    first commit

---
+++ src/main/java/com/se/simplerepo/SimplerepoApplication.java
@@ -0,0 +1,13 @@
+package com.se.simplerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplerepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplerepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/simplerepo/SimplerepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.simplerepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplerepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
